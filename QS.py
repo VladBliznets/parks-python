188047dd1c9117fa3ac4cfa331565093ad638090
VladBliznets <99480189+VladBliznets@users.noreply.github.com>
2024-04-17 03:32:12 +0300

    Update QS.py

--- QS.py
+++ QS.py
@@ -12,47 +12,38 @@ class Solver:
         print("Job Started")
         k = len(self.workers)
         print("Workers %d" % k)
-
         arr = self.read_input()
         n = len(arr)
 
-        # Розподіл задач між робітниками
         mapped = []
         for i in range(k):
             print("map %d" % i)
             mapped.append(self.workers[i].mymap(arr[(n * i // k): (n * (i + 1) // k)]))
-
-        # Зведення результатів
         result = self.myreduce(mapped)
-
         self.write_output(result)
-
         print("Job Finished")
 
     @staticmethod
     @expose
     def mymap(arr):
-        # Мапування частини масиву        
         if len(arr) <= 1:
             return arr
-        return quick_sort(arr)
+        return Solver.quick_sort(arr)
 
     @staticmethod
     @expose
     def myreduce(mapped):
-        # Зведення відсортованих частин масиву
         return list(heapq.merge(part.value for part in mapped))
 
     @staticmethod
     def quick_sort(arr):
-        # Алгоритм швидкого сортування        
         if len(arr) <= 1:
             return arr
         pivot = arr[len(arr) // 2]
         left = [x for x in arr if x < pivot]
         middle = [x for x in arr if x == pivot]
         right = [x for x in arr if x > pivot]
-        return quick_sort(left) + middle + quick_sort(right)
+        return Solver.quick_sort(left) + middle + Solver.quick_sort(right)
 
     def read_input(self):
         with open(self.input_file_name, 'r') as f:
